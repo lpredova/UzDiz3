9c787581f911f92ea2b77366bfbcc9fbfd2a70fb
Tomislav Novosel <to.novosel@gmail.com>
2016-01-04 12:46:15 +0100

    Implemented option 2

--- T2_01_zadaca_3/src/CompositeIterator/FileTreeIterator.java
+++ T2_01_zadaca_3/src/CompositeIterator/FileTreeIterator.java
@@ -6,22 +6,26 @@
 package CompositeIterator;
 
 import FileStructureComposite.AppFile;
+import java.util.ArrayList;
 
 /**
  * Composite iterator for walking through file tree composite
+ *
  * @author tonovosel
  */
 public class FileTreeIterator implements Container {
-    
+
+    ArrayList<String> elementsData = new ArrayList<>();
+
     @Override
     public Iterator getIterator() {
         return new CreatedTreeIterator();
     }
-    
+
     private class CreatedTreeIterator implements Iterator {
-        
+
         private int index;
-        
+
         @Override
         public boolean hasNext(AppFile parent) {
             int ChildrenNum = parent.getChildren().size();
@@ -30,7 +34,7 @@ public class FileTreeIterator implements Container {
             }
             return false;
         }
-        
+
         @Override
         public Object getNextChild(AppFile parent) {
             if (this.hasNext(parent)) {
@@ -38,16 +42,16 @@ public class FileTreeIterator implements Container {
             }
             return null;
         }
-        
+
     }//inner class
-    
+
     /**
      * Recursion for printing out file tree composite
-     * @param elem 
+     *
+     * @param elem
      */
-
     public void printStructure(AppFile elem) {
-        
+
         FileTreeIterator ft = this;
         for (Iterator iter = ft.getIterator(); iter.hasNext(elem);) {
             AppFile nextElement = (AppFile) iter.getNextChild(elem);
@@ -56,7 +60,31 @@ public class FileTreeIterator implements Container {
                 printStructure(nextElement);
             }
         }
-        
+
     }
-    
+
+    /**
+     * Method that returns elements data in list
+     *
+     * @param elem
+     * @return
+     */
+    public ArrayList<String> getElementData(AppFile elem) {
+
+        FileTreeIterator ft = this;
+        for (Iterator iter = ft.getIterator(); iter.hasNext(elem);) {
+            AppFile nextElement = (AppFile) iter.getNextChild(elem);
+            elementsData.add("Name: " + nextElement.getName());
+            elementsData.add("Type: " + nextElement.getType());
+            elementsData.add("Created at: " + nextElement.getCreatedAt());
+            elementsData.add("Updated at: " + nextElement.getUpdatedAt());
+            elementsData.add("Size: " + nextElement.getFormattedSize());
+            elementsData.add("-------------------");
+            if (nextElement.getType().equals("directory") && !nextElement.getChildren().isEmpty()) {
+                getElementData(nextElement);
+            }
+        }
+        return elementsData;
+    }
+
 }
