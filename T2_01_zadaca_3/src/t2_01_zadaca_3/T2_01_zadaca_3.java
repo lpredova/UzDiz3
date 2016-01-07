9059a5a6cd4b4f1cefe46ef386dd6d151e4bdbef
Toni Steyskal <toni.steyskal@gmail.com>
2016-01-07 12:10:28 +0100

    Testing Output Removed

--- T2_01_zadaca_3/src/t2_01_zadaca_3/T2_01_zadaca_3.java
+++ T2_01_zadaca_3/src/t2_01_zadaca_3/T2_01_zadaca_3.java
@@ -48,12 +48,7 @@ public class T2_01_zadaca_3 {
         //Reading initial file structure - creational iterator, has only one iteration and creates dir 
         filesRepository.getIterator(args[3]);
         
-        root = filesRepository.directoryTree.get(0);
-        
-        for (Object file : filesRepository.directoryTree.toArray()) {
-            AppFile af = (AppFile) file;
-            System.out.println(af.getAbsolutePath());
-        }
+        root = filesRepository.directoryTree.get(0);       
         
         View v = new View(rowNum, colNum, screenDivision);
         Model m = new Model();
